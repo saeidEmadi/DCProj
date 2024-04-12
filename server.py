f30cd7a546f29594e04a53770bc69761f2c64297
saeid <emadisaeidabad@gmail.com>
2024-04-12 02:45:14 -0700

    server initial and Threading programming

---
+++ server.py
@@ -0,0 +1,9 @@
+import threading
+
+class Server(threading.Thread):
+    """ Server Class use for receive report and data from cameras """
+    def __init__(self, serverIp, ):
+        pass
+    
+    def run(self):
+        pass
