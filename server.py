fb89a082fca7acb628f794fdc9bd993ff4182bb7
saeid <emadisaeidabad@gmail.com>
2024-04-12 03:13:54 -0700

    get config paramaters

--- server.py
+++ server.py
@@ -1,9 +1,21 @@
 import threading
-
+import configparser
+from typing import Final
 class Server(threading.Thread):
     """ Server Class use for receive report and data from cameras """
-    def __init__(self, serverIp : str, serverPort : int, backlogListen : int, \
-        DEBUG : bool = False):
+    # read config file and initial config variables
+    config = configparser.ConfigParser()
+    try : 
+        config.read('config.ini')
+    except :
+        raise FileExistsError("config.ini file is not exists.")
+    
+    # default value for listen socket backLog    
+    backlogListenVal : Final[int] = config['Server']["backlogListenVal"]
+    
+    def __init__(self, serverIp : str = config['Server']['server IP'], \
+            serverPort : int = int(config['Server']['port']), \
+            DEBUG : bool = False):
         """ initial class variables """
         pass
     
