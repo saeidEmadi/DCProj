ac9d3fc83a80f4d60078e3ff07183cac0fcf3c78
saeid <emadisaeidabad@gmail.com>
2024-04-12 02:58:00 -0700

    initial method Paramaterize

--- server.py
+++ server.py
@@ -2,11 +2,19 @@ import threading
 
 class Server(threading.Thread):
     """ Server Class use for receive report and data from cameras """
-    def __init__(self, serverIp, ):
+    def __init__(self, serverIp : str, serverPort : int, backlogListen : int, \
+        DEBUG : bool = False):
+        """ initial class variables """
+        pass
+    
+    def stream(self):
+        """ receive video from camera """
         pass
     
     def run(self):
+        """ run server """
         pass
+    
     
 if __name__ == "__main__":
     """ this class, you can use this \
