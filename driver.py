7aa274e978b6fee2f4ed10ed1affaf907dc2abc0
saeid <emadisaeidabad@gmail.com>
2024-04-14 05:32:10 -0700

    detection param definition

--- driver.py
+++ driver.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     argparser.add_argument('port', metavar = 'port', type = int, nargs = 1, help = "port number")
     argparser.add_argument('-v', '--yolov', metavar = 'yoloVersion', type = str, nargs = 1, help = "Yolo pre-Train Model version (default : "+config['Traffic Camera']['yoloVersion']+")",default = config['Traffic Camera']['yoloVersion'])
     argparser.add_argument('-BB', '--bb', action = "store_true", help = "Draw A Bounding Box for each object")
-    argparser.add_argument('-d', '--detect', metavar = 'detectObjects', default = 'vehicles', type = str, nargs = 1, choices=['all', 'vehicles'], help = "detect all objects in ms-COCO or only vehicles (choices : [all,vehicles], default = vehicles)")
+    argparser.add_argument('-d', '--detect', metavar = 'coco class name\'s', default = 'vehicles', nargs = '*', help = "detect all objects in ms-COCO or only vehicles (example : vehicles person [etc.]], default = vehicles)")
     argparser.add_argument('--test', action = "store_true", help = "flag for Enable defaults parameters run :: \n "+config['Server']['server IP']+" "+config['Server']['port']+" -v "+config['Traffic Camera']['yoloVersion']+", -c 5")
     argparser.add_argument('--stream', action = "store_true", help = "stream traffic camera real-Time")
     argparser.add_argument('--debug', action = "store_true", help = "flag for Enable Debug mode [show CLI logs]")
