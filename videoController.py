29c60c0921a99fdc25dda4dca1e99a087a46dafd
saeid <emadisaeidabad@gmail.com>
2024-04-20 17:30:59 -0700

    video Controller app

---
+++ videoController.py
@@ -0,0 +1,24 @@
+class VideoController():
+    """ this class use for control video versions """
+    def __init__(self):
+        """ initial class constructor """
+        pass
+    
+    def videoAdder(self) -> bool :
+        """ add video from source to driver Path """
+        pass
+    
+    def __driverChecker(self) -> bool :
+        """ check driver.py for running | 
+            True : run app and add video, etc
+            False : add this script to driver path 
+                    or add path """
+        pass
+    
+    def __nameChanger(self, nameStr) -> str :
+        """ change video name """
+        pass
+    
+    def checkPairVideoAndClient(self) -> bool :
+        """ check pair camera's and Client's """
+        pass
